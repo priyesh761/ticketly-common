9c4596d877b69a6c129d0378de212d05f7734503
Priyesh Shetty <shettypriyesh761@gmail.com>
2025-01-29 22:08:31 +0530

    Add NATS publisher + listener to common lib.
    - Make base listner and base publisher class.
    - Make base listener and publisher generic with schema extending `event`
      interface.
    - This event interface extending schema will enforce subject and
      corresponding data for onMessage event listener.

---
+++ src/events/base-publisher.ts
@@ -0,0 +1,18 @@
+import { Stan } from "node-nats-streaming";
+import { Event } from "./events";
+
+export abstract class Publisher<T extends Event> {
+  abstract subject: T["subject"];
+
+  constructor(private client: Stan) {}
+
+  async publish(data: T["data"]): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.client.publish(this.subject, JSON.stringify(data), (err) => {
+        if (err) reject(err);
+        console.log("Event published to subject", this.subject);
+        resolve();
+      });
+    });
+  }
+}
